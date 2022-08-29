8e16fd762101d0e916e78d3a3b75fd712e58f94f
Kim <rodrigkim@gmail.com>
2022-08-29 16:55:23 -0400

    commit tree 5bb8b87db15f14a2a78fc0203980da4fbcd7ca34

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 4;
+const num2 = 15.5;
+const num3 = 16;
+const num4 = 20;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+const mod = num4 % num3;
+
+const max = Math.max(num1, num2, num3, num4);
